4348864b971cebd35dbce314ed0affd60ad0a3b2
bigBabie19 <146693435+bigBabie19@users.noreply.github.com>
2023-11-16 21:03:07 +0700

    Create Jawaban6.sql

---
+++ UTS/Jawaban6.sql
@@ -0,0 +1,9 @@
+-- nomor 6
+SELECT mahasiswa.id_mk,
+CONCAT_WS(" ",mahasiswa.first_name,mahasiswa.last_name) AS `Nama Mahasiswa`,
+mahasiswa.nim AS `NIM`, mahasiswa.fakultas AS `Fakultas`,
+CONCAT_WS(" ",dosen.first_name,dosen.last_name) AS `Nama Dosen`,
+mata_kuliah.nama_mk AS `Mata Kuliah`, mata_kuliah.sks
+FROM mahasiswa
+INNER JOIN dosen ON mahasiswa.id_mk = dosen.id_mk
+LEFT JOIN mata_kuliah on mahasiswa.id_mk = mata_kuliah.id_mk; 
