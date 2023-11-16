da1b0d27edfe3c1b39f5f616d209cb718925ef28
bigBabie19 <146693435+bigBabie19@users.noreply.github.com>
2023-11-16 22:09:14 +0700

    Update Jawaban4.sql

--- UTS/Jawaban4.sql
+++ UTS/Jawaban4.sql
@@ -1,6 +1,7 @@
 -- nomor 4
-SELECT mahasiswa.nim,
+SELECT mahasiswa.nim AS `NIM`,
 CONCAT_WS(" ", mahasiswa.first_name, mahasiswa.last_name) AS `Full Name`,
-mahasiswa.fakultas, mata_kuliah.nama_mk
+mahasiswa.fakultas AS `Fakultas`,
+mata_kuliah.nama_mk AS `Mata Kuliah`
 FROM mahasiswa
 INNER JOIN mata_kuliah ON mahasiswa.id_mk = mata_kuliah.id_mk;
