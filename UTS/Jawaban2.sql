90812a808e79e645769c1a12328cf284cd34811d
bigBabie19 <146693435+bigBabie19@users.noreply.github.com>
2023-11-16 21:06:46 +0700

    Create Jawaban2.sql

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,15 @@
+-- nomor 2
+INSERT INTO mata_kuliah 
+VALUES
+(1, 'Basis Data', 'Ilmu Komputer', '3'),
+(2, 'Sistem Operasi', 'Ilmu Komputer', '3');
+
+INSERT INTO mahasiswa
+VALUES
+(1, '20220801001', 'John', 'Son', 'Ilmu Komputer'),
+(2, '20220801002', 'Den', 'Nis', 'Ilmu Komputer');
+
+INSERT INTO dosen 
+VALUES
+(1, 'Bran', 'Don', 'Ilmu Komputer'),
+(2, 'John', 'Wick', 'Ilmu Komputer');
