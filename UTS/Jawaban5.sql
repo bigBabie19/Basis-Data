c9d34ed5950566c851f4ba24264b9b72fb8bcbb1
bigBabie19 <146693435+bigBabie19@users.noreply.github.com>
2023-11-16 22:09:43 +0700

    Update Jawaban5.sql

--- UTS/Jawaban5.sql
+++ UTS/Jawaban5.sql
@@ -1,6 +1,8 @@
 -- nomor 5
-SELECT dosen.fakultas,
+SELECT 
 CONCAT_WS(" ",dosen.first_name,dosen.last_name) AS `Full Name`,
-mata_kuliah.nama_mk, mata_kuliah.sks
+dosen.fakultas AS `Fakultas`,
+mata_kuliah.nama_mk AS `Mata Kuliah`, 
+mata_kuliah.sks
 FROM dosen
 INNER JOIN mata_kuliah ON dosen.id_mk = mata_kuliah.id_mk;
